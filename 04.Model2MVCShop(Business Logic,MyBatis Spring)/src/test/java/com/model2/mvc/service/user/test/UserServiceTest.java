7e84fae30c99db98d074ec1bd03e398862b14960
sean <sean@localhost>
2023-03-28 20:50:43 +0900

    Pusgh please

--- 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/test/java/com/model2/mvc/service/user/test/UserServiceTest.java
+++ 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/test/java/com/model2/mvc/service/user/test/UserServiceTest.java
@@ -93,7 +93,7 @@ public class UserServiceTest {
 		Assert.assertNotNull(userService.getUser("user05"));
 	}
 	
-	//@Test
+	@Test
 	 public void testUpdateUser() throws Exception{
 		 
 		User user = userService.getUser("testUserId");
